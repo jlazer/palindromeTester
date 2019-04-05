4899be570fa09eca1d06524e3c3415cbab2b0960
Justin Lazarski <blackhawks189@gmail.com>
2019-04-05 16:27:50 -0500

    The program is done except for a exiting the loop upon a blank line being entered.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,32 +21,40 @@ public class Main {
         reverseInput = "";
         // Prompt user to input a string
         System.out.println("Please input a string");
-        // Place user input into the userInput variable
-        userInput = input.nextLine();
-        System.out.println("Input: " + userInput);
+        while (input.hasNextLine()) {
 
-        String strippedInput = userInput;
-        // Convert strippedInput to ignore case, spaces, non-words, and digits.
-        strippedInput = strippedInput.toLowerCase();
-        strippedInput = strippedInput.replaceAll(" ", "");
-        strippedInput = strippedInput.replaceAll("\\W", "");
-        strippedInput = strippedInput.replaceAll("\\d", "");
-        // Print the strippedInput
-        System.out.println("Test input: " + strippedInput);
+            // Place user input into the userInput variable
+            userInput = input.nextLine();
+            System.out.println("Input: " + userInput);
 
-        int length = strippedInput.length();
-        // Reverse the strippedInput Variable and store it in the reverseInput variable
-        // This for loop starts at index 2 and will iterate through the userInput variable until it reaches 0 while placing them into reverseInput
-        for(int i = strippedInput.length() - 1; i >= 0; i--) {
-            reverseInput = reverseInput + strippedInput.charAt(i);
-        }
-        //System.out.println(reverseInput);
-        System.out.println("ReverseInput: " + reverseInput);
-        // This if else statement checks if userInput is equal to reverseInput
-        if (strippedInput.equals(reverseInput)) {
-            System.out.println("String is a palindrome");
-        } else {
-            System.out.println("String is not a palindrome");
+            String strippedInput = userInput;
+            // Convert strippedInput to ignore case, spaces, non-words, and digits.
+            strippedInput = strippedInput.toLowerCase();
+            strippedInput = strippedInput.replaceAll(" ", "");
+            strippedInput = strippedInput.replaceAll("\\W", "");
+            strippedInput = strippedInput.replaceAll("\\d", "");
+            // Print the strippedInput
+            System.out.println("Test input: " + strippedInput);
+
+            int length = strippedInput.length();
+            // Reverse the strippedInput Variable and store it in the reverseInput variable
+            // This for loop starts at index 2 and will iterate through the userInput variable until it reaches 0 while placing them into reverseInput
+            for (int i = strippedInput.length() - 1; i >= 0; i--) {
+                reverseInput = reverseInput + strippedInput.charAt(i);
+            }
+            //System.out.println(reverseInput);
+            System.out.println("ReverseInput: " + reverseInput);
+            // This if else statement checks if userInput is equal to reverseInput
+            if (strippedInput.equals(reverseInput)) {
+                System.out.println("String is a palindrome");
+            } else {
+                System.out.println("String is not a palindrome");
+            }
+            userInput = "";
+            strippedInput = "";
+            reverseInput = "";
+            System.out.println("Please input a string.");
+
         }
     }
 }
